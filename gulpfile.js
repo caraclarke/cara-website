7c53b6bb77047a5e9f1913c2f38ca1c2beed6b9f
caraclarke <cara.clarke8@gmail.com>
2017-04-26 13:51:49 -0400

    add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,79 @@
-var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cssnano = require('gulp-cssnano'),
-    jshint = require('gulp-jshint'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
-    notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload'),
-    del = require('del'); // TODO: switch to autoload?
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync').create();
+var cache = require('gulp-cache');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var gulpIf = require('gulp-if');
+var imagemin = require('gulp-imagemin');
+var runSequence = require('run-sequence');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var useref = require('gulp-useref');
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  });
+});
+
+// call to clear caches off local system
+gulp.task('cache:clear', function(cb) {
+  return cache.clearAll(cb);
+});
+
+// delete dist folder
+// doesnt delete cached image folder
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('images', function() {
+  return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('sass', function() {
+  return gulp.src('src/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('useref', function() {
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    // minifies only if js file
+    .pipe(gulpIf('*.js', uglify()))
+    // minifies only if css file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function(cb) {
+  runSequence('clean:dist',
+    [ 'sass', 'useref', 'images', 'fonts'], cb);
+});
+
+gulp.task('default', function(cb) {
+  runSequence(['sass', 'browserSync', 'watch'], cb);
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  // scss watcher
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  // html watcher
+  gulp.watch('src/*.html', browserSync.reload);
+  // js watcher
+  gulp.watch('src/js/**/*.js', browserSync.reload);
+});
